53a8a41b409cb13b1f725c31870467014ae55bf0
pkg4446 <pkg4446@gmail.com>
2024-09-06 17:08:37 +0900

    240906

---
+++ js/20_animation.js
@@ -0,0 +1,121 @@
+import * as THREE from "../core/build/three.module.js"
+import { OrbitControls } from "../core/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "../core/jsm/loaders/GLTFLoader.js"
+
+class App {
+    constructor() {
+        const divContainer = document.querySelector("#webgl-container");
+        this._divContainer = divContainer;
+        
+        const renderer = new THREE.WebGLRenderer({antialias:true});
+        renderer.setPixelRatio(window.devicePixelRatio);
+        divContainer.appendChild(renderer.domElement);
+        this._renderer = renderer;
+
+        const scene = new THREE.Scene();
+        scene.background = new THREE.Color(0xffffff);
+        this._scene = scene;
+
+        this._setCamera();
+        this._setLight();
+        this._setModel();
+        this._setControls();
+
+        window.onresize = this.resize.bind(this);
+        this.resize();
+
+        requestAnimationFrame(this.render.bind(this));
+    }
+
+    _setCamera() {
+        const width  = this._divContainer.clientWidth;
+        const height = this._divContainer.clientHeight;
+        const camera = new THREE.PerspectiveCamera(
+            75,
+            width/height,
+            100,
+            1000
+        );
+        camera.position.set(0, 0, 300);
+        this._camera = camera;
+    }
+
+    _setLight() {
+        const color = 0xffffff;
+        const intensity = 5;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(0, 0, 1);
+        this._scene.add(light);
+    }
+
+    _setupAnimations(gltf) {
+        const model = gltf.scene;
+        //const mixer = new THREE.AnimationMixer(model);
+        const gltfAnimations = gltf.animations;
+        // const domControls = document.querySelector("#controls");
+        // const animationsMap = {};
+
+        gltfAnimations.forEach(animationClip => {
+            const name = animationClip.name;
+            console.log(name);
+            /*
+            const domButton = document.createElement("div");
+            domButton.classList.add("button");
+            domButton.innerText = name;
+            domControls.appendChild(domButton);
+
+            domButton.addEventListener("click", () => {
+                const animationName = domButton.innerHTML;
+                this.changeAnimation(animationName);
+            });
+
+            const animationAction = mixer.clipAction(animationClip);
+            animationsMap[name] = animationAction;
+            */
+        });
+        
+        // this._mixer = mixer;
+        // this._animationsMap = animationsMap;
+        // this._currentAnimationAction =  this._animationsMap["Idle"];
+        // this._currentAnimationAction.play();
+    }
+
+    _setModel() {
+        new GLTFLoader().load("../data/GLB/model.glb", (gltf) => {
+            const model = gltf.scene;
+            this._scene.add(model);
+            
+            this._setupAnimations(gltf);
+        });
+    }
+///////////////////////////////////////////////////////////
+    _setControls() {
+        new OrbitControls(this._camera, this._divContainer)
+    }
+
+    resize() {
+        const width  = this._divContainer.clientWidth;
+        const height = this._divContainer.clientHeight;
+
+        this._camera.aspect = width/height;
+        this._camera.updateProjectionMatrix();
+
+        this._renderer.setSize(width, height);
+    }
+
+    render(time) {
+        this._renderer.render(this._scene, this._camera);
+        this.update(time);
+        requestAnimationFrame(this.render.bind(this));
+    }
+
+    update(time){
+        time *= 0.001; // second unitc
+        //this._cube.rotation.x = time;
+        //this._cube.rotation.y = time;
+    }
+}
+
+window.onload = function() {
+    new App();
+}
