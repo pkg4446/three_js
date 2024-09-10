715fc4857632d7e5478ff53b107211788b7a0386
pkg4446 <pkg4446@gmail.com>
2024-09-10 14:35:46 +0900

    240910

--- js/20_animation.js
+++ js/20_animation.js
@@ -50,15 +50,15 @@ class App {
 
     _setupAnimations(gltf) {
         const model = gltf.scene;
-        //const mixer = new THREE.AnimationMixer(model);
+        const mixer = new THREE.AnimationMixer(model);
         const gltfAnimations = gltf.animations;
-        // const domControls = document.querySelector("#controls");
-        // const animationsMap = {};
+        const domControls = document.querySelector("#controls");
+        const animationsMap = {};
 
         gltfAnimations.forEach(animationClip => {
             const name = animationClip.name;
             console.log(name);
-            /*
+            
             const domButton = document.createElement("div");
             domButton.classList.add("button");
             domButton.innerText = name;
@@ -71,13 +71,12 @@ class App {
 
             const animationAction = mixer.clipAction(animationClip);
             animationsMap[name] = animationAction;
-            */
         });
         
-        // this._mixer = mixer;
-        // this._animationsMap = animationsMap;
-        // this._currentAnimationAction =  this._animationsMap["Idle"];
-        // this._currentAnimationAction.play();
+        this._mixer = mixer;
+        this._animationsMap = animationsMap;
+        this._currentAnimationAction =  this._animationsMap["Idle"];
+        this._currentAnimationAction.play();
     }
 
     _setModel() {
@@ -103,6 +102,16 @@ class App {
         this._renderer.setSize(width, height);
     }
 
+    changeAnimation(animationName) {
+        const previousAnimationAction = this._currentAnimationAction;
+        this._currentAnimationAction = this._animationsMap[animationName];
+
+        if(previousAnimationAction !== this._currentAnimationAction) {
+            previousAnimationAction.fadeOut(0.5);
+            this._currentAnimationAction.reset().fadeIn(0.5).play();
+        }
+    }
+
     render(time) {
         this._renderer.render(this._scene, this._camera);
         this.update(time);
@@ -111,8 +120,11 @@ class App {
 
     update(time){
         time *= 0.001; // second unitc
-        //this._cube.rotation.x = time;
-        //this._cube.rotation.y = time;
+        if(this._mixer) {
+            const deltaTime = time - this._previousTime;
+            this._mixer.update(deltaTime);
+        }
+        this._previousTime = time;
     }
 }
 
