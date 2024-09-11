5b175afedd551ad242fcd212bce7ca53abfb4a43
pkg4446 <pkg4446@gmail.com>
2024-09-11 17:09:11 +0900

    240911

---
+++ js/00_basic.js
@@ -0,0 +1,88 @@
+import * as THREE from "../core/build/three.module.js"
+import { OrbitControls } from "../core/jsm/controls/OrbitControls.js";
+
+class App {
+    constructor() {
+        const divContainer = document.querySelector("#webgl-container");
+        this._divContainer = divContainer;
+        
+        const renderer = new THREE.WebGLRenderer({antialias:true});
+        renderer.setPixelRatio(window.devicePixelRatio);
+        divContainer.appendChild(renderer.domElement);
+        this._renderer = renderer;
+
+        const scene = new THREE.Scene();
+        this._scene = scene;
+
+        this._setCamera();
+        this._setLight();
+        this._setModel();
+        this._setControls();
+
+        window.onresize = this.resize.bind(this);
+        this.resize();
+
+        requestAnimationFrame(this.render.bind(this));
+    }
+
+    _setCamera() {
+        const width  = this._divContainer.clientWidth;
+        const height = this._divContainer.clientHeight;
+        const camera = new THREE.PerspectiveCamera(
+            75,
+            width/height,
+            0.1,
+            100
+        );
+        camera.position.z = 2;
+        this._camera = camera;
+    }
+
+    _setLight() {
+        const color = 0xffffff;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color,intensity);
+        light.position.set(-1,2,4);
+        this._scene.add(light);
+    }
+
+    _setModel() {
+        const geometry = new THREE.BoxGeometry(1,1,1);
+        const material = new THREE.MeshPhongMaterial({color:0x44aa88});
+
+        const cube = new THREE.Mesh(geometry,material);
+
+        this._scene.add(cube);
+        this._cube = cube;
+    }
+///////////////////////////////////////////////////////////
+    _setControls() {
+        new OrbitControls(this._camera, this._divContainer)
+    }
+
+    resize() {
+        const width  = this._divContainer.clientWidth;
+        const height = this._divContainer.clientHeight;
+
+        this._camera.aspect = width/height;
+        this._camera.updateProjectionMatrix();
+
+        this._renderer.setSize(width, height);
+    }
+
+    render(time) {
+        this._renderer.render(this._scene, this._camera);
+        this.update(time);
+        requestAnimationFrame(this.render.bind(this));
+    }
+
+    update(time){
+        time *= 0.001; // second unitc
+        //this._cube.rotation.x = time;
+        //this._cube.rotation.y = time;
+    }
+}
+
+window.onload = function() {
+    new App();
+}
--- js/01_basic.js
+++ js/01_basic.js
@@ -1,88 +1,91 @@
-import * as THREE from "../core/build/three.module.js"
-import { OrbitControls } from "../core/jsm/controls/OrbitControls.js";
+import * as THREE from '../build/three.module.js';
+import { OrbitControls } from "../examples/jsm/controls/OrbitControls.js"
 
 class App {
     constructor() {
+        this._setupThreeJs();
+        this._setupCamera();
+        this._setupLight();
+        this._setupModel();
+        this._setupControls();
+        this._setupEvents();        
+    }
+
+    _setupThreeJs() {
         const divContainer = document.querySelector("#webgl-container");
         this._divContainer = divContainer;
-        
-        const renderer = new THREE.WebGLRenderer({antialias:true});
+
+        const renderer = new THREE.WebGLRenderer({ antialias: true });
         renderer.setPixelRatio(window.devicePixelRatio);
         divContainer.appendChild(renderer.domElement);
         this._renderer = renderer;
 
         const scene = new THREE.Scene();
         this._scene = scene;
-
-        this._setCamera();
-        this._setLight();
-        this._setModel();
-        this._setControls();
-
-        window.onresize = this.resize.bind(this);
-        this.resize();
-
-        requestAnimationFrame(this.render.bind(this));
     }
 
-    _setCamera() {
-        const width  = this._divContainer.clientWidth;
-        const height = this._divContainer.clientHeight;
+    _setupCamera() {
         const camera = new THREE.PerspectiveCamera(
-            75,
-            width/height,
-            0.1,
+            75, 
+            window.innerWidth / window.innerHeight, 
+            0.1, 
             100
         );
+
         camera.position.z = 2;
         this._camera = camera;
     }
 
-    _setLight() {
+    _setupLight() {
         const color = 0xffffff;
         const intensity = 1;
-        const light = new THREE.DirectionalLight(color,intensity);
-        light.position.set(-1,2,4);
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
         this._scene.add(light);
     }
 
-    _setModel() {
-        const geometry = new THREE.BoxGeometry(1,1,1);
-        const material = new THREE.MeshPhongMaterial({color:0x44aa88});
-
-        const cube = new THREE.Mesh(geometry,material);
-
+    _setupModel() {
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const material = new THREE.MeshPhongMaterial({color: 0x44a88});
+        const cube = new THREE.Mesh(geometry, material);
         this._scene.add(cube);
-        this._cube = cube;
-    }
-///////////////////////////////////////////////////////////
-    _setControls() {
-        new OrbitControls(this._camera, this._divContainer)
     }
 
-    resize() {
-        const width  = this._divContainer.clientWidth;
-        const height = this._divContainer.clientHeight;
-
-        this._camera.aspect = width/height;
-        this._camera.updateProjectionMatrix();
-
-        this._renderer.setSize(width, height);
+    _setupControls() {
+        this._orbitControls = new OrbitControls(this._camera, this._divContainer);
     }
 
-    render(time) {
-        this._renderer.render(this._scene, this._camera);
-        this.update(time);
+    _setupEvents() {
+        window.onresize = this.resize.bind(this);
+        this.resize();
+
+        this._clock = new THREE.Clock();
         requestAnimationFrame(this.render.bind(this));
     }
 
-    update(time){
-        time *= 0.001; // second unitc
-        //this._cube.rotation.x = time;
-        //this._cube.rotation.y = time;
+    update() {
+        const delta = this._clock.getDelta();
+        this._orbitControls.update();
+    }
+
+    render() {
+        this._renderer.render(this._scene, this._camera);   
+        this.update();
+
+        requestAnimationFrame(this.render.bind(this));
+    }
+
+    resize() {
+        const width = this._divContainer.clientWidth;
+        const height = this._divContainer.clientHeight;
+
+        this._camera.aspect = width / height;
+        this._camera.updateProjectionMatrix();
+        
+        this._renderer.setSize(width, height);
     }
 }
 
-window.onload = function() {
+window.onload = function () {
     new App();
 }
